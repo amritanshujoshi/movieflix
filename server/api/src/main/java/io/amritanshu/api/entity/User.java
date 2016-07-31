d32e73624d733f409f34eaa3c56045be5b7c3e6b
amritanshujoshi <amritanshujoshi@gmail.com>
2016-07-31 17:32:13 -0500

    feat(module-api): add annotations(entity, table and queries) to user class fix(module-api): change userId to id feat(module-api): add setter functions for member variables

--- server/api/src/main/java/io/amritanshu/api/entity/User.java
+++ server/api/src/main/java/io/amritanshu/api/entity/User.java
@@ -1,17 +1,27 @@
 package io.amritanshu.api.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 
+@Entity
+@Table
+@NamedQueries({
+	@NamedQuery(name = "User.findByUsername", query = "SELECT u FROM User u WHERE u.username=:pUsername"),
+	@NamedQuery(name = "User.findAll", query = "SELECT u FROM User u ORDER BY u.firstName ASC")
+})
 public class User {
 
 	@Id
 	@GenericGenerator(name = "customUUID", strategy = "uuid2")
 	@GeneratedValue(generator = "customUUID")
-	private String userId;
+	private String id;
 
 	@Column(unique = true)
 	private String username;
@@ -28,7 +38,7 @@ public class User {
 	private String role = "User";
 
 	public String getId() {
-		return userId;
+		return id;
 	}
 
 	public String getUsername() {
@@ -62,10 +72,47 @@ public class User {
 	public String getRole() {
 		return role;
 	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
 
 	@Override
 	public String toString() {
-		return "Movie [id=" + userId + ", username=" + username + ", firstName=" + firstName + ", lastName=" + lastName
+		return "Movie [id=" + id + ", username=" + username + ", firstName=" + firstName + ", lastName=" + lastName
 				+ ", city=" + city + ", state=" + state + ", zip=" + zipCode + "]";
 	}
 }
