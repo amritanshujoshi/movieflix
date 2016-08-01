ebd0ac8f293a8e2ac2dc33a705fa7e84b20f3fae
amritanshujoshi <amritanshujoshi@gmail.com>
2016-07-31 20:39:08 -0400

    fix(module-api): change http status code

--- server/api/src/main/java/io/amritanshu/api/exception/UserNotFoundException.java
+++ server/api/src/main/java/io/amritanshu/api/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@ package io.amritanshu.api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code=HttpStatus.BAD_REQUEST)
+@ResponseStatus(code=HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = -7206718542049875922L;
