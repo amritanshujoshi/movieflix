25ae3aee3dd62f5de7b61eb16a518aa654527ee4
amritanshujoshi <amritanshujoshi@gmail.com>
2016-07-31 20:10:17 -0400

    fix(module-api): change http status

--- server/api/src/main/java/io/amritanshu/api/exception/UserExistsException.java
+++ server/api/src/main/java/io/amritanshu/api/exception/UserExistsException.java
@@ -3,7 +3,7 @@ package io.amritanshu.api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code=HttpStatus.NOT_FOUND)
+@ResponseStatus(code=HttpStatus.BAD_REQUEST)
 public class UserExistsException extends RuntimeException{
 
 	private static final long serialVersionUID = -7706774887174686475L;
