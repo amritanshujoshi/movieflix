4ef05a260ed135e10647f987bd83a4e37b79d9de
amritanshujoshi <amritanshujoshi@gmail.com>
2016-08-03 01:41:23 -0400

    feat(module-api): modify create method to find and add review using movie and user id

--- server/api/src/main/java/io/amritanshu/api/repository/ReviewRepositoryImpl.java
+++ server/api/src/main/java/io/amritanshu/api/repository/ReviewRepositoryImpl.java
@@ -6,15 +6,26 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import io.amritanshu.api.entity.Movie;
 import io.amritanshu.api.entity.Review;
+import io.amritanshu.api.entity.User;
+import io.amritanshu.api.exception.MovieNotFoundException;
+import io.amritanshu.api.exception.UserNotFoundException;
 
 @Repository
 public class ReviewRepositoryImpl implements ReviewRepository{
 	
 	@PersistenceContext
 	private EntityManager em;
+	
+	@Autowired
+	MovieRepository movieRepository;
+	
+	@Autowired
+	UserRepository userRepository;
 
 	@Override
 	public List<Review> findByMovieId(String movieId) {
@@ -28,7 +39,19 @@ public class ReviewRepositoryImpl implements ReviewRepository{
 	}
 
 	@Override
-	public Review create(Review review) {
+	public Review create(String movie_id, String user_id, Review review) {
+		Movie mExisting = movieRepository.findOne(movie_id);
+		User uExisting = userRepository.findOne(user_id);
+		if(mExisting == null) {
+			throw new MovieNotFoundException("");
+		}
+		if(uExisting == null) {
+			throw new UserNotFoundException("");
+		}
+		mExisting.getReviews().add(review);
+		uExisting.getReviews().add(review);
+		review.setUser(uExisting);
+		review.setMovie(mExisting);
 		em.persist(review);
 		return review;
 	}
