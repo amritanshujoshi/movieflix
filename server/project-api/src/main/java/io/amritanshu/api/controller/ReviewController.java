d4dce0185b25f4d67461ea16cdaf67c3d2b09504
amritanshujoshi <amritanshujoshi@gmail.com>
2016-08-06 19:40:24 -0400

    fix(module-api): modify controller methods to fetch and create reviews

--- server/project-api/src/main/java/io/amritanshu/api/controller/ReviewController.java
+++ server/project-api/src/main/java/io/amritanshu/api/controller/ReviewController.java
@@ -20,14 +20,19 @@ public class ReviewController {
 	@Autowired
 	ReviewService reviewService;
 	
-	@RequestMapping(method = RequestMethod.GET, path = "{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public List<Review> findByMovieId(@PathVariable("id") String movieId) {
-		return reviewService.findByMovieId(movieId);
+	@RequestMapping(method = RequestMethod.GET, path = "{title}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public List<Review> findByMovieTitle(@PathVariable("title") String movie) {
+		return reviewService.findByMovieTitle(movie);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, path = "{movie}/{user}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public Review create(@PathVariable("movie") String movie_id, @PathVariable("user") String user_id, @RequestBody Review review) {
-		return reviewService.create(movie_id, user_id, review);
+	@RequestMapping(method = RequestMethod.GET, path = "averageRating/{title}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Double getAverageRating(@PathVariable("title") String movie) {
+		return reviewService.getAverageRating(movie);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, path = "{movie_id}/{user_id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Review create(@PathVariable("movie_id") String movie_Id, @PathVariable("user_id") String user_Id, @RequestBody Review review) {
+		return reviewService.create(movie_Id, user_Id, review);
 	}
 
 }
