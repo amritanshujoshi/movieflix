eddceff1543941a0d4ea3d40c600ff095c0c1958
amritanshujoshi <amritanshujoshi@gmail.com>
2016-08-13 14:12:03 -0400

    feat(module-layout): init gulp for project

---
+++ layout/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+
+gulp.task('sometask', function () {
+    //get source files
+    //perform actions
+    //put results somewhere
+});
+
+gulp.task('styles', function () {
+    //get source files
+    //perform actions
+    //put results somewhere
+});
+
+gulp.task('scripts', function () {
+    //get source files
+    //perform actions
+    //put results somewhere
+});
