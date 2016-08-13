d2bc06e2b269802d918d27dc69884e983ec5ff82
amritanshujoshi <amritanshujoshi@gmail.com>
2016-08-13 14:11:37 -0400

    feat(module-layout): init app.js for project

--- layout/project-layout/app/app.js
+++ layout/project-layout/app/app.js
@@ -1,3 +1,32 @@
-/**
- * Created by Amritanshu on 8/12/2016.
- */
+(function () {
+    'use strict';
+
+    angular
+        .module('movieflix', ['ngRoute', 'ngMessages'])
+        .config(moduleConfig);
+
+    function moduleConfig($routeProvider) {
+
+        $routeProvider
+            .when('/user/movies', {
+                templateUrl: 'app/user/views/movies.tmpl.html',
+                controller: 'MoviesController',
+                controllerAs: 'moviesVm'
+            })
+            .otherwise({
+                redirectTo: '/users/movies'
+            });
+            /*.when('/user/movie/:id', {
+                templateUrl: 'app/user/views/movie-detail.tmpl.html',
+                controller: "MovieDetailsController",
+                controllerAs: 'movieDetailVm'
+            })
+            .when('/admin/movies', {
+                templateUrl: 'app/admin/views/movies.tmpl.html',
+                controller: "MoviesAdmController",
+                controllerAs: "moviesAdmVm"
+            })
+            .when('/admin/movie/')*/
+    }
+
+})();
