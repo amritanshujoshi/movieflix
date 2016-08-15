246788e40c9d57c437d89125d6e7cead682b8f96
amritanshujoshi <amritanshujoshi@gmail.com>
2016-08-14 21:16:09 -0400

    feat(module-layout): add editable movies list for admin

--- layout/project-layout/app/admin/controllers/movies.controller.js
+++ layout/project-layout/app/admin/controllers/movies.controller.js
@@ -1,3 +1,45 @@
-/**
- * Created by Amritanshu on 8/13/2016.
- */
+(function () {
+    'use strict';
+
+    angular.module('movieflix')
+        .controller('MoviesAdminController', MoviesAdminController);
+
+    MoviesAdminController.$inject = ['movieService', '$routeParams'];
+
+    function MoviesAdminController(movieService, $routeParams) {
+        var moviesAdminVm = this;
+
+        moviesAdminVm.changeSort = changeSort;
+
+        init();
+
+        function init() {
+
+            moviesAdminVm.sorter = {
+                by: 'title',
+                reverse: false
+            };
+
+            movieService
+                .getMovies()
+                .then(function (movies) {
+                    moviesAdminVm.movies = movies;
+                }, function (error) {
+                    console.log(error);
+                });
+
+            movieService
+                .deleteMovie($routeParams.title, $routeParams)
+                .then(function (movie) {
+                    console.log(movie.title + ' has been deleted!!');
+                }, function (error) {
+                    console.log(error);
+                });
+        }
+
+        function changeSort(prop) {
+            moviesAdminVm.sorter.by = prop;
+            moviesAdminVm.sorter.reverse = !moviesAdminVm.sorter.reverse;
+        }
+    }
+})();
