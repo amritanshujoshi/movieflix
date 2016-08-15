075f4bd5948853dddea0699dcfc886df59013c96
amritanshujoshi <amritanshujoshi@gmail.com>
2016-08-14 21:20:49 -0400

    feat(module-layout): add movie details and admin controls

--- layout/project-layout/app/app.js
+++ layout/project-layout/app/app.js
@@ -8,13 +8,28 @@
     function moduleConfig($routeProvider) {
 
         $routeProvider
+            .when('/login', {
+                templateUrl: 'app/user/views/signup.tmpl.html',
+                controller: 'SignupController',
+                controllerAs: 'signupVm'
+            })
             .when('/user/movies', {
                 templateUrl: 'app/user/views/movies.tmpl.html',
                 controller: 'MoviesController',
                 controllerAs: 'moviesVm'
             })
+            .when('/user/movies/:title', {
+                templateUrl: 'app/user/views/movie-detail.tmpl.html',
+                controller: 'MovieDetailController',
+                controllerAs: 'movieDetailVm'
+            })
+            .when('/admin/movies', {
+                templateUrl: 'app/admin/views/movies.tmpl.html',
+                controller: 'MoviesAdminController',
+                controllerAs: 'moviesAdminVm'
+            })
             .otherwise({
-                redirectTo: '/user/movies'
+                redirectTo: '/admin/movies'
             });
             /*.when('/user/movie/:id', {
                 templateUrl: 'app/user/views/movie-detail.tmpl.html',
