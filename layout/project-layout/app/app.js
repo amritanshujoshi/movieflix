a9d4cd9501dfc33f9c90083b4b5071e48c29f2bc
amritanshujoshi <amritanshujoshi@gmail.com>
2016-08-15 00:42:04 -0400

    feat(module-layout): add routing for app features

--- layout/project-layout/app/app.js
+++ layout/project-layout/app/app.js
@@ -10,6 +10,11 @@
         $routeProvider
             .when('/login', {
                 templateUrl: 'app/user/views/signup.tmpl.html',
+                controller: 'LoginController',
+                controllerAs: 'loginVm'
+            })
+            .when('/signup', {
+                templateUrl: 'app/user/views/signup.tmpl.html',
                 controller: 'SignupController',
                 controllerAs: 'signupVm'
             })
@@ -17,6 +22,26 @@
                 templateUrl: 'app/user/views/movies.tmpl.html',
                 controller: 'MoviesController',
                 controllerAs: 'moviesVm'
+            })
+            .when('/user/movies/topRated', {
+                templateUrl: 'app/user/views/movies-top-rated.tmpl.html',
+                controller: 'MoviesTopRatedController',
+                controllerAs: 'moviesTopRatedVm'
+            })
+            .when('/user/movies/topVoted', {
+                templateUrl: 'app/user/views/movies-top-voted.tmpl.html',
+                controller: 'MoviesTopVotedController',
+                controllerAs: 'moviesTopVotedVm'
+            })
+            .when('/user/series/topRated', {
+                templateUrl: 'app/user/views/series-top-rated.tmpl.html',
+                controller: 'SeriesTopRatedController',
+                controllerAs: 'seriesTopRatedVm'
+            })
+            .when('/user/series/topVoted', {
+                templateUrl: 'app/user/views/series-top-voted.tmpl.html',
+                controller: 'SeriesTopVotedController',
+                controllerAs: 'seriesTopVotedVm'
             })
             .when('/user/movies/:title', {
                 templateUrl: 'app/user/views/movie-detail.tmpl.html',
@@ -29,19 +54,19 @@
                 controllerAs: 'moviesAdminVm'
             })
             .otherwise({
-                redirectTo: '/admin/movies'
+                redirectTo: '/user/movies'
             });
-            /*.when('/user/movie/:id', {
-                templateUrl: 'app/user/views/movie-detail.tmpl.html',
-                controller: "MovieDetailsController",
-                controllerAs: 'movieDetailVm'
-            })
-            .when('/admin/movies', {
-                templateUrl: 'app/admin/views/movies.tmpl.html',
-                controller: "MoviesAdmController",
-                controllerAs: "moviesAdmVm"
-            })
-            .when('/admin/movie/')*/
+        /*.when('/user/movie/:id', {
+         templateUrl: 'app/user/views/movie-detail.tmpl.html',
+         controller: "MovieDetailsController",
+         controllerAs: 'movieDetailVm'
+         })
+         .when('/admin/movies', {
+         templateUrl: 'app/admin/views/movies.tmpl.html',
+         controller: "MoviesAdmController",
+         controllerAs: "moviesAdmVm"
+         })
+         .when('/admin/movie/')*/
     }
 
 })();
